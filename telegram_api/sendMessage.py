d3442dc462c0012fd31cf4519c8825a10357c22b
nigoraxongadoyboyeva <nigoraxon_gadoyboyeva@mail.ru>
2021-02-19 17:18:03 +0500

    'sendmessage'

--- telegram_api/sendMessage.py
+++ telegram_api/sendMessage.py
@@ -4,6 +4,9 @@ from pprint import pprint
 token = '1686665754:AAHyBYlpebtmliH256xnQW7_7J4D2nJRtjo'
 
 url=f'https://api.telegram.org/bot{token}/sendMessage'
-#id=665551552
-r = requests.get(url)
-
+payload={
+    'chat_id':480822009,
+    'text':'YOMON AYIQ, QUNGIR PUNGIR AYIQ'
+}
+r = requests.get(url,params=payload)
+print(r.json())
